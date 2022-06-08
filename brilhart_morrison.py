2083fbc5162389f28ab29b8548c81eb484ed48ab
Maksym <maksim.skorobagatko@gmail.com>
2022-06-09 01:43:19 +0300

    Update brilhart_morrison.py

--- brilhart_morrison.py
+++ brilhart_morrison.py
@@ -11,7 +11,7 @@ def Factor_Baza(n):
     Baza = [-1]
     p = 2
     p_supremum = m.exp(pow(m.log2(n) * m.log2(m.log2(n)), 0.5)
-    for k in range(0, 10):
+    for k in range(10):
         while p < p_supremum:
             if test_div_test(p) == bool(True): #число просте
             if lejandr(n, p) == bool(True): #число квадратичний лишок
@@ -27,4 +27,4 @@ def brilhart_morrison_test(n):
    alpha_vektor = [m.sqrt(n)]
    a_vektor = [int(alpha_vektor[-1])]
    u_vektor = []
-    
+   
